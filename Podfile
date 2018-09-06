cf42adcfe56e43982549adca394b816a8acbefc1
Ryan Davis <ryan.davis@bison.howard.edu>
2018-09-06 07:52:46 -0400

    added everything

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flix_app_2' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flix_app_2
+	pod 'AlamofireImage', '~> 3.3'
+  target 'flix_app_2Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'flix_app_2UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ flix_app_2/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flix_app_2
+//
+//  Created by Ryan Davis on 9/5/18.
+//  Copyright © 2018 Ryan Davis. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
